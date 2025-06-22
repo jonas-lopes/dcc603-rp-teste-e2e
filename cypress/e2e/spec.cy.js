862d44524e57521d9939f21d4291efd094bcb8e1
Jonas F. Lopes <jonas.lopes@uemg.br>
2025-06-22 20:46:00 -0300

    Adiciona novos testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,55 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Não adiciona tarefa vazia', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+        .type('{enter}');
+
+    cy.get('[data-cy=todos-list]')
+        .children()
+        .should('have.length', 0);
+  });
+
+  it('Adiciona múltiplas tarefas e verifica a ordem', () => {
+    cy.visit('');
+
+    const tarefas = ['Estudar Laravel', 'Estudar React', 'Estudar Docker'];
+    tarefas.forEach(tarefa => {
+      cy.get('[data-cy=todo-input]').type(`${tarefa}{enter}`);
+    });
+
+    cy.get('[data-cy=todos-list]')
+        .children()
+        .should('have.length', 3)
+        .then(items => {
+          expect(items[0]).to.contain.text('Estudar Laravel');
+          expect(items[1]).to.contain.text('Estudar React');
+          expect(items[2]).to.contain.text('Estudar Docker');
+        });
+  });
+
+  it('Marca e desmarca uma tarefa como concluída', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+        .type('Estudar Cypress{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+        .check()
+        .should('be.checked');
+
+    cy.get('[data-cy=todos-list] > li')
+        .should('have.class', 'completed'); // se sua app aplica classe CSS
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+        .uncheck()
+        .should('not.be.checked');
+
+    cy.get('[data-cy=todos-list] > li')
+        .should('not.have.class', 'completed');
+  });
+
 });
